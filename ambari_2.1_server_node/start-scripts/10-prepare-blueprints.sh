841a0b4697f3776d541902da5f647d1ef71c1721
rfraposa <rich@hortonworks.com>
2015-06-15 19:30:04 -0600

    Removed blueprints - changes repo to HDP 2.2

--- ambari_2.1_server_node/start-scripts/10-prepare-blueprints.sh
+++ ambari_2.1_server_node/start-scripts/10-prepare-blueprints.sh
@@ -3,5 +3,6 @@
 #Wait a bit to ensure that Ambari server is fully up and running
 sleep 20 
 
-/root/scripts/install_cluster.sh $BLUEPRINT_BASE
+#I am commenting this out so that the blueprints don't install. I want to do a wizard install
+#/root/scripts/install_cluster.sh $BLUEPRINT_BASE
 
